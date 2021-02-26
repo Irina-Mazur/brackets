7f3f15b11c85f29e4a363009b2bd071a6ee1cf59
Irina-Mazur <irin.mazur@gmail.com>
2021-02-26 21:40:02 +0300

    feat: test the task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    bracketsConfig = bracketsConfig.reduce((acc,val) => {
+        acc[val[0]] = val[1];
+        return acc;
+    },{});
+  
+    for (let i = 0; i < str.length; i++) {
+        for (let key in bracketsConfig) {
+          if (key === bracketsConfig[key] && stack.length && stack[stack.length - 1] === key) {
+              stack.pop();
+              continue;
+            }
+    
+            if ( str[i] === key ) {
+                stack.push(str[i]);
+                break;
+            }
+    
+            if (str[i] === bracketsConfig[key]) {
+                if (stack[stack.length-1] === key) {
+                    stack.pop();
+                    break;
+                } 
+                else {
+                    return false;
+                }
+            }
+        }
+      
+      }
+    return !stack.length;
 }
+  
+  
+  
